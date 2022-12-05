260b9263243c0883a31ac34f63488e4662807827
peerapol411 <peerapol.onm@gmail.com>
2022-12-05 20:31:09 +0700

    workplace

--- lib/main.dart
+++ lib/main.dart
@@ -45,11 +45,13 @@ class _MyHomePageState extends State<MyHomePage> {
       'https://i.dlpng.com/static/png/5459272-games-games-icon-circle-png-free-transparent-png-download-pngkey-circle-game-png-820_730_preview.png';
   String watchSeries =
       'https://toppng.com/uploads/preview/netflix-logo-png-11593869496jqso5gxgsy.png';
-  String listenMusic = 'https://cdn-icons-png.flaticon.com/512/3844/3844724.png';
+  String listenMusic =
+      'https://cdn-icons-png.flaticon.com/512/3844/3844724.png';
   int indexSelected = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Container(
           alignment: Alignment.center,
@@ -57,186 +59,11 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: SingleChildScrollView(
-        child: indexSelected == 3
-            ? Container(
-                child: Column(
-                  children: [
-                    Stack(
-                      children: [
-                        Container(
-                          color: Colors.grey[200],
-                          width: double.infinity,
-                          height: 240,
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 20),
-                          alignment: Alignment.center,
-                          child: CircleAvatar(
-                            radius: 100.0,
-                            backgroundImage: AssetImage(
-                              urlProfile,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                      child: Text(
-                        'Peerapol Onmanee',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    Divider(
-                      indent: 50,
-                      endIndent: 50,
-                    ),
-                    Text(
-                      'peerapol.work@gmail.com | github.com/peerapol411',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.grey[600],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 5, bottom: 10),
-                      child: Text(
-                        '+66 62-459-3907',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 35, bottom: 10),
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Social : ',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 35, bottom: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Image.network(
-                            fbImg,
-                            height: 25,
-                          ),
-                          Text(
-                            '   Peerapol Onmanee  ',
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 20),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(5.0),
-                              child: Image.network(
-                                twtImg,
-                                height: 25,
-                              ),
-                            ),
-                          ),
-                          Text(
-                            '   @OPeerius',
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 35, bottom: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(5.0),
-                            child: Image.network(
-                              linkedImg,
-                              height: 25,
-                            ),
-                          ),
-                          Text(
-                            '   Peerapol Onmanee',
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 15),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(5.0),
-                              child: Image.network(
-                                igImg,
-                                height: 35,
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'peerapol.on',
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 35, bottom: 10),
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Hobby : ',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 35, bottom: 10),
-                      child: Row(
-                        children: [
-                          Image.network(
-                            playGames,
-                            height: 40,
-                          ),
-                          Text('Play games'),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 35, bottom: 10),
-                      child: Row(
-                        children: [
-                          Image.network(
-                            watchSeries,
-                            height: 45,
-                          ),
-                          Text('Watch movies'),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 42, bottom: 10),
-                      child: Row(
-                        children: [
-                          Image.network(
-                            listenMusic,
-                            height: 30,
-                          ),
-                          Text('  Listern to music'),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            : Container(),
-      ),
+          child: indexSelected == 3
+              ? profile()
+              : indexSelected == 0
+                  ? workplace()
+                  : Container()),
       bottomNavigationBar: BottomNavigationBar(
         // ignore: prefer_const_literals_to_create_immutables
         items: [
@@ -273,5 +100,289 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       indexSelected = index;
     });
+  }
+
+  profile() {
+    return Container(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                color: Colors.grey[200],
+                width: double.infinity,
+                height: 240,
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                alignment: Alignment.center,
+                child: CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage: AssetImage(
+                    urlProfile,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Peerapol Onmanee',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Divider(
+            indent: 50,
+            endIndent: 50,
+          ),
+          Text(
+            'peerapol.work@gmail.com | github.com/peerapol411',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: Colors.grey[600],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 5, bottom: 10),
+            child: Text(
+              '+66 62-459-3907',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 35, bottom: 10),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Social : ',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 35, bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.network(
+                  fbImg,
+                  height: 25,
+                ),
+                Text(
+                  '   Peerapol Onmanee  ',
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 20),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(5.0),
+                    child: Image.network(
+                      twtImg,
+                      height: 25,
+                    ),
+                  ),
+                ),
+                Text(
+                  '   @OPeerius',
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 35, bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5.0),
+                  child: Image.network(
+                    linkedImg,
+                    height: 25,
+                  ),
+                ),
+                Text(
+                  '   Peerapol Onmanee',
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 15),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(5.0),
+                    child: Image.network(
+                      igImg,
+                      height: 35,
+                    ),
+                  ),
+                ),
+                Text(
+                  'peerapol.on',
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 35, bottom: 10),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Hobby : ',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 35, bottom: 10),
+            child: Row(
+              children: [
+                Image.network(
+                  playGames,
+                  height: 40,
+                ),
+                Text('Play games'),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 35, bottom: 10),
+            child: Row(
+              children: [
+                Image.network(
+                  watchSeries,
+                  height: 45,
+                ),
+                Text('Watch movies'),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 42, bottom: 10),
+            child: Row(
+              children: [
+                Image.network(
+                  listenMusic,
+                  height: 30,
+                ),
+                Text('  Listern to music'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String axons =
+      'https://siamrath.co.th/files/styles/1140/public/img/20220907/01987e5341a4d96173c1464576bfef3dafd81559f6fb6683554a6aec6403d1e1.jpg?itok=dUWwrGZ4';
+  String cpf = 'https://www.42bangkok.com/wp-content/uploads/2021/11/CPF.png';
+
+  workplace() {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Image.network(
+                axons,
+                height: 125,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Image.network(
+                cpf,
+                height: 105,
+              ),
+            ),
+          ],
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 15),
+          child: Text(
+            'Currently, I working for CPF IT Center (AXONS) in fullstack developer position',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 10),
+          child: SizedBox(
+            width: 320,
+            height: 2,
+            child: Container(
+              color: Colors.black12,
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 20, top: 20),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'About My Work',
+            textAlign: TextAlign.left,
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 20, top: 10),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'I develope application in Farm Business Delivery team. the application support farmer, researcher, husbandry. that application about breeding chicken for the best food we made.',
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 20, top: 20),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Experience',
+            textAlign: TextAlign.left,
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 20, top: 10),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'I used to do a cooperative at Efin X Company Limited which work about Fintech. I worked for web developer that display about stock derivatives in realtime and make algorithm to decide and notifly about buy, sell sign (butterfly algorithm).',
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 20, top: 10),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'I used to join ACM competitive thailand 2019, online round.',
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 20),
+          child: SizedBox(
+            width: 320,
+            height: 2,
+            child: Container(
+              color: Colors.black12,
+            ),
+          ),
+        ),
+        Container(
+          width: 290,
+          margin: EdgeInsets.only(left: 40, top: 20),
+          alignment: Alignment.center,
+          child: Text(
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+            'If you can control the situation, That means you will not get any better.',
+          ),
+        ),
+      ],
+    );
   }
 }
